d4772916b832e231b1a724c4e4915b08d901ec0f
Peyton Walters <pawalt@hey.com>
2022-07-10 14:51:24 -0700

    accept routes

--- golinks/scripts/start.sh
+++ golinks/scripts/start.sh
@@ -3,5 +3,5 @@
 export PATH=$PATH:/app/
 
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=golinks
+/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=golinks --accept-routes
 /app/golinks
